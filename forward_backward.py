3599a0c9da36413e9cf5c0c3fb2d50237bf2f1de
zahra dehghanian <z.dehghanian@aut.ac.ir>
2021-01-05 15:03:18 +0330

    add dataset

--- forward_backward.py
+++ forward_backward.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 class MyHmm(object):  # base class for different HMM models
@@ -55,24 +54,25 @@ class MyHmm(object):  # base class for different HMM models
                 for y in self.states:
                     landa[t][y] = (self.fwd[t][y] * self.bwk[t][y]) / p_obs
                     if t == 0: self.pi[y] = landa[t][y]
-                    if t == len(obs) - 1:continue
+                    if t == len(obs) - 1: continue
                     phi[t][y] = {}
                     for y1 in self.states:
-                        phi[t][y][y1] = round(self.fwd[t][y] * self.A[y][y1] * self.B[y1][obs[t + 1]] * self.bwk[t + 1][y1] / p_obs , 4)
+                        phi[t][y][y1] = round(
+                            self.fwd[t][y] * self.A[y][y1] * self.B[y1][obs[t + 1]] * self.bwk[t + 1][y1] / p_obs, 4)
             # Maximization step : compute A and B
             for y in self.states:
                 # compute A
                 for y1 in self.states:
                     val = sum([phi[t][y][y1] for t in range(len(obs) - 1)])  #
                     val /= sum([landa[t][y] for t in range(len(obs) - 1)])
-                    self.A[y][y1] = round(val,4)
+                    self.A[y][y1] = round(val, 4)
                 # compute B
                 for k in self.symbols:
                     val = 0.0
                     for t in range(len(obs)):
                         if obs[t] == k: val += landa[t][y]
                     val /= sum([landa[t][y] for t in range(len(obs))])
-                    self.B[y][k] = round(val,4)
+                    self.B[y][k] = round(val, 4)
         return
 
     def viterbi(self, obs):
@@ -99,25 +99,24 @@ class MyHmm(object):  # base class for different HMM models
         return (prob, path[state])
 
 
-seq0 = ('Heads', 'Heads', 'Heads')
-seq1 = ('Heads', 'Heads', 'Tails')
-seq2 = ('Heads', 'Tails', 'Heads')
-seq3 = ('Heads', 'Tails', 'Tails')
-seq4 = ('Tails', 'Heads', 'Heads')
-seq5 = ('Tails', 'Heads', 'Tails')
-seq6 = ('Tails', 'Tails', 'Heads')
-seq7 = ('Tails', 'Tails', 'Tails')
-observation_list = [seq0, seq1, seq2, seq3, seq4, seq5, seq6, seq7]
-observations = seq6 + seq0 + seq7 + seq1  # you can set this variable to any arbitrary length of observations
-print("Learning the model through Forward-Backward Algorithm for the observations", observations)
-models_dir = os.path.join('.', 'models')
-model_file = os.path.join(models_dir,"coins1.json")
-hmm = MyHmm(os.path.join('.', model_file))
+def get_observation(file_obs):
+    file = open(file_obs)
+    obs = []
+    for line in file:
+        words = line.split(",")
+        val = words[1].split("\n")[0]
+        obs.append(val)
+    return obs
 
-iteration = [1,5,10]
+
+observations = get_observation("Test2.txt")
+print("Learning the model through Forward-Backward Algorithm for the observations", observations)
+hmm = MyHmm("equal.json")
+
+iteration = [1, 5, 10]
 for iter in iteration:
-    hmm.forward_backward(observations , iter)
-    print("The new model parameters after "+str(iter)+" iteration are: ")
+    hmm.forward_backward(observations, iter)
+    print("The new model parameters after " + str(iter) + " iteration are: ")
     print("A = ", hmm.A)
     print("B = ", hmm.B)
     print("pi = ", hmm.pi)
